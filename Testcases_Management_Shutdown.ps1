05fe9cdaacbe46227834856c291e9958872f3a78
vsowjanya <vsowjanya@vmware.com>
2021-04-22 09:18:41 -0700

    adding testcases

---
+++ Testcases_Management_Shutdown.ps1
@@ -0,0 +1,30 @@
+#Shut Down the vRealize Automation Virtual Machines in the Management Domain
+
+#Shut Down the Cross-Region Workspace ONE Access Virtual Machines in the Management Domain
+
+#Shut down the cross-region Workspace ONE Access cluster by using the vRealize Suite Lifecycle Manager administration interface.[Read more]
+
+#Shut Down the vRealize Suite Lifecycle Manager Virtual Machine in the Management Domain
+
+#Shut down the vRealize Suite Lifecycle Manager virtual machine in the management domain.[Read more]
+
+#Shut Down the vRealize Log Insight Virtual Machines in the Management Domain
+
+#Shut down the three vRealize Log Insight nodes in the analytics cluster in the management domain .[Read more]
+
+#Shut Down the Region-Specific Workspace ONE Access Virtual Machine in the Management Domain
+
+#Shut down the region-specific Workspace ONE Access virtual machine in the management domain.[Read more]
+
+#Shut Down the NSX-T Edge Nodes in the Management Domain
+ShutdownStartup-SDDCComponent -server sfo-m01-vc01.sfo.rainpole.io -nodes sfo-m01-en01, sfo-m01-en02 -user administrator@vsphere.local -pass VMw@re123! -timeout 600
+
+#Shut Down the SDDC Manager Virtual Machine in the Management Domain
+
+#Shut down the SDDC Manager virtual machine in the management domain.[Read more]
+
+#Set DRS Automation Level of the Management Domain to Manual
+
+#Shut Down the vSphere Cluster Services Virtual Machines in the Management Domain
+
+#Shut Down vSAN and the ESXi Hosts in the Management Domain
