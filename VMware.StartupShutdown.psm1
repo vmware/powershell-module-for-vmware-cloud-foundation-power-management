f70d6aff0ee26ae17a1b337159596962125047c6
vsowjanya <vsowjanya@vmware.com>
2021-04-30 02:04:46 -0700

    added all additional fuxtions

--- VMware.StartupShutdown.psm1
+++ VMware.StartupShutdown.psm1
@@ -41,7 +41,7 @@
                     foreach ($node in $nodes) {
 			            $count=0
             
-                        $vm_obj = Get-VMGuest -VM $node
+                        $vm_obj = Get-VMGuest -server $server -VM $node
                         Write-Output $node, $vm_obj
 			            if($vm_obj.State -eq 'NotRunning'){
 				            Write-Output "The VM $vm_obj is already in powered off state"
@@ -49,8 +49,8 @@
 				            continue
 
 			            }
-			            Stop-VMGuest -VM $node -Confirm:$false
-                        $vm_obj = Get-VMGuest -VM $node
+			            Stop-VMGuest -server $server -VM $node -Confirm:$false
+                        $vm_obj = Get-VMGuest -server $server -VM $node
                         Write-Output $vm_obj.State
                         Start-Sleep -Seconds 10
             
@@ -59,7 +59,7 @@
 			                Start-Sleep -Seconds 1
                             Write-Output "Sleeping for 1 second"
 				            $count = $count + 1
-                            $vm_obj = Get-VMGuest -VM $node
+                            $vm_obj = Get-VMGuest -server $server -VM $node
                             Write-Output $vm_obj.State
 
 			            }
@@ -76,7 +76,7 @@
                     foreach ($node in $nodes) {
 			            $count=0
             
-                        $vm_obj = Get-VMGuest -VM $node
+                        $vm_obj = Get-VMGuest -server $server -VM $node
                         Write-Output $node, $vm_obj
 			            if($vm_obj.State -eq 'Running'){
 				            Write-Output "The VM $vm_obj is already in powered on state"
@@ -85,7 +85,7 @@
 
 			            }
 			            Start-VM -VM $node -Confirm:$false
-                        $vm_obj = Get-VMGuest -VM $node
+                        $vm_obj = Get-VMGuest -server $server -VM $node
                         Write-Output $vm_obj.State
                         Start-Sleep -Seconds 10
             
@@ -94,7 +94,7 @@
 			                Start-Sleep -Seconds 1
                             Write-Output "Sleeping for 1 second"
 				            $count = $count + 1
-                            $vm_obj = Get-VMGuest -VM $node
+                            $vm_obj = Get-VMGuest -server $server -VM $node
                             Write-Output $vm_obj.State
 
 			            }
@@ -149,7 +149,7 @@ Function ShutdownStartup-ComponentOnHost {
             [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String]$user,
             [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String]$pass,
 		    [Int]$timeout,
-            [String[]]$pattern="",
+            [String]$pattern,
             [String]$task='Shutdown'
     )
 
@@ -158,8 +158,8 @@ Function ShutdownStartup-ComponentOnHost {
 	    Connect-VIServer -Server $server -Protocol https -User $user -Password $pass
         Write-Output $server, $user, $pass, $nodes, $timeout
         #$nodes = Get-VM | select Name, PowerState | where Name -match $pattern
-        if ($pattern -ne "") {
-             $nodes = Get-VM | where Name -match $pattern | select Name, PowerState, VMHost | where VMHost -match $server
+        if ($pattern) {
+             $nodes = Get-VM -server $server | where Name -match $pattern | select Name, PowerState, VMHost | where VMHost -match $server
         } else {
             $nodes = @()
         }
@@ -172,7 +172,7 @@ Function ShutdownStartup-ComponentOnHost {
             foreach ($node in $nodes) {
 			    $count=0
                 
-                $vm_obj = Get-VMGuest -VM $node.Name | where VmUid -match $server
+                $vm_obj = Get-VMGuest -server $server -VM $node.Name | where VmUid -match $server
                 Write-Output $vm_obj
 			    if($vm_obj.State -eq 'NotRunning'){
 				    Write-Output "The VM $vm_obj is already in powered off state"
@@ -181,8 +181,8 @@ Function ShutdownStartup-ComponentOnHost {
 
 			    }
         
-			    Get-VMGuest -VM $node.Name | where VmUid -match $server | Stop-VMGuest -Confirm:$false
-                $vm_obj = Get-VMGuest -VM $node.Name | where VmUid -match $server
+			    Get-VMGuest -server $server -VM $node.Name | where VmUid -match $server | Stop-VMGuest -Confirm:$false
+                $vm_obj = Get-VMGuest -server $server -VM $node.Name | where VmUid -match $server
                 Write-Output $vm_obj.State
             
                 #Get-VMGuest -VM $node | where $_.State -eq "NotRunning"
@@ -190,7 +190,7 @@ Function ShutdownStartup-ComponentOnHost {
 			        Start-Sleep -Seconds 1
                     Write-Output "Sleeping for 1 second"
 				    $count = $count + 1
-                    $vm_obj = Get-VMGuest -VM $node.Name | where VmUid -match $server
+                    $vm_obj = Get-VMGuest  -VM $node.Name | where VmUid -match $server
                     Write-Output $vm_obj.State
 
 			    }
@@ -201,6 +201,8 @@ Function ShutdownStartup-ComponentOnHost {
 				    Write-Output "The VM is successfully shutdown"
 			    }
 		    }
+		} elseif ($pattern) {
+			Write-Output "There are no VM's matching the pattern"
 	    } else {
             Write-Output "No Node is specified. Hence shutting down the ESXi host"
             Stop-VMHost -VMHost $server -Server $server -confirm:$false -RunAsync
@@ -212,7 +214,7 @@ Function ShutdownStartup-ComponentOnHost {
 				foreach ($node in $nodes) {
 					$count=0
 					
-					$vm_obj = Get-VMGuest -VM $node.Name | where VmUid -match $server
+					$vm_obj = Get-VMGuest -server $server -VM $node.Name | where VmUid -match $server
 					Write-Output $vm_obj
 					if($vm_obj.State -eq 'Running'){
 						Write-Output "The VM $vm_obj is already in powered on state"
@@ -222,7 +224,7 @@ Function ShutdownStartup-ComponentOnHost {
 			    }
         
 			    Start-VM -VM $nodes.Name
-                $vm_obj = Get-VMGuest -VM $node.Name | where VmUid -match $server
+                $vm_obj = Get-VMGuest -server $server -VM $node.Name | where VmUid -match $server
                 Write-Output $vm_obj.State
             
                 #Get-VMGuest -VM $node | where $_.State -eq "NotRunning"
@@ -230,7 +232,7 @@ Function ShutdownStartup-ComponentOnHost {
 			        Start-Sleep -Seconds 1
                     Write-Output "Sleeping for 1 second"
 				    $count = $count + 1
-                    $vm_obj = Get-VMGuest -VM $node.Name | where VmUid -match $server
+                    $vm_obj = Get-VMGuest -server $server -VM $node.Name | where VmUid -match $server
                     Write-Output $vm_obj.State
 
 			    }
@@ -241,6 +243,8 @@ Function ShutdownStartup-ComponentOnHost {
 				    Write-Output "The VM is successfully started"
 			    }
 		    }
+		} elseif ($pattern) {
+			Write-Output "There are no VM's matching the pattern"
 	    } else {
             Write-Output "No Node is specified. Hence Starting the ESXi host"
             Start-VMHost -VMHost $server -Server $server -confirm:$false -RunAsync
@@ -648,7 +652,67 @@ Function StartStop-VAMIServiceStatus {
     }
 }
 
-Function Set-DrsAutomationLevel {
+
+Function Get-EnvironmentId {
+ <#
+        .NOTES
+        ===========================================================================
+        Created by:		Sowjanya V
+        Date:			03/16/2021
+        Organization:	VMware
+        ===========================================================================
+        
+        .SYNOPSIS
+        Cross Region Envionment or globalenvironment Id of any product in VRSLCM need to be found
+    
+        .DESCRIPTION
+        This is to fetch right environment id for cross region env, which is needed for shutting down the VRA components via VRSLCM
+		This also fetches the global environment id for VIDM
+    
+        .EXAMPLE
+        PS C:\>Get_EnvironmentId -host <VRSLCM> -user <username> -pass <password> -product <VRA/VROPS/VRLI/VIDM>
+        This example shows how to fetch environment id for cross region VRSLCM
+        Sample URL formed on TB04 is as shown below -- vcfadmin@local VMw@re123!
+        Do Get on https://xreg-vrslcm01.rainpole.io/lcm/lcops/api/v2/environments
+           and findout what is the id assosiated with Cross-Region Environment      
+
+    #>
+          
+    Param (
+        [Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$host,
+        [Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$user,
+        [Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$pass,
+		[Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Name
+    )
+    
+    Try {
+		$Global:myHeaders = createHeader $user $pass
+		#write-output $Global:myHeaders
+        $uri = "https://$host/lcm/lcops/api/v2/environments"
+		#write-output "1. $uri"
+        $response = Invoke-RestMethod -Method GET -URI $uri -headers $myHeaders -ContentType application/json 
+		#write-output "2." $response
+        $env_id = $response  | foreach-object -process { if($_.environmentName -match $Name) { $_.environmentId }} 
+        #write-output "3." $env_id
+		return $env_id
+    }
+    Catch {
+       $PSItem.InvocationInfo
+    }
+}
+
+
+
+Function ShutdownStartupXVIDM-ViaVRSLCM
+{
     <#
         .NOTES
         ===========================================================================
@@ -658,48 +722,185 @@ Function Set-DrsAutomationLevel {
         ===========================================================================
         
         .SYNOPSIS
-        Set the automation level to manual or fully automated 
+        Shutting down VRA or VROPS via VRSLCM
     
         .DESCRIPTION
-        Set the automation level to manual or fully automated 
+        There are no POWER CLI or POWER SHELL cmdlet to power off the products via VRSLCM. Hence writing my own
     
         .EXAMPLE
-        PS C:\>Set-DrsAutomationLevel -Server $server -User $user  -Pass $pass -cluster <clustername> -level <Manual/FullyAutomated>
+        PS C:\> ShutdownProduct_ViaVRSLCM -host <VRSLCM> -user <username> -pass <password> -product <VRA/VROPS/VRLI/VIDM> -mode <power-on/power-off>
+        This example shutsdown a product via VRSLCM
+        Sample URL formed on TB04 is as shown below
+        Do Get on https://xreg-vrslcm01.rainpole.io/lcm/lcops/api/v2/environments
+           and findout what is the id assosiated with Cross-Region Environment 
+
+        $uri = "https://xreg-vrslcm01.rainpole.io/lcm/lcops/api/v2/environments/Cross-Region-Env1612043838679/products/vra/deployed-vms"
+
+       
 
     #>
-	Param (
-            [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String]$server,
-            [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String]$user,
-            [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String]$pass,
-			[Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$cluster,
-			[Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$level
+          
+    Param (
+        [Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$host,
+		[Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$user,
+		[Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$pass,
+		[Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$mode,
+        [Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$product
 
     )
-
+    
     Try {
-        Connect-VIServer -Server $server -Protocol https -User $user -Password $pass
-		$out = Get-Cluster -Name $cluster
-		if ($out.DrsAutomationLevel -eq $level) {
-			write-Output "DrsAutomationLevel is already set to $level"
+        $env_id = Get-EnvironmentId -host $host -user $user -pass $pass -Name 'globalenvironment'
+		
+		write-output $env_id
+		
+		$Global:myHeaders = createHeader vcfadmin@local VMw@re123!
+		write-output $myHeaders
+		if($product -match 'health') {
+			$uri = "https://$host/lcm/lcops/api/v2/environments/$env_id/health-check"
 		} else {
-			$task =  set-cluster -Cluster $cluster -DrsAutomationLevel $level -confirm:$false 
-			if($task.DrsAutomationLevel -eq $level) {
-				write-Output "DrsAutomationLevel is set to $level successfully"
-			} else {
-				write-Output "DrsAutomationLevel could not be set to $level"
+			$uri = "https://$host/lcm/lcops/api/v2/environments/$env_id/products/$product/$mode"
+		}
+		
+		write-output $uri
+        $json = {}
+        $response = Invoke-RestMethod -Method POST -URI $uri -headers $myHeaders -ContentType application/json -body $json
+		
+		write-output "------------------------------------"
+		write-output $response
+		write-output "------------------------------------"
+        if($response.requestId) {
+            Write-Output "Successfully initiated shutdown of the product $product"
+        } else {
+            Write-Error "Unable to shutdown $product due to response "
+        }
+		$id = $response.requestId
+		$uri2 = "https://$host/lcm/request/api/v2/requests/$id"
+		$count = 0
+		$timeout = 900
+		while($count -le $timeout) {
+			$count = $count + 60
+			start-sleep -s 60
+			$response2 = Invoke-RestMethod -Method GET -URI $uri2 -headers $myHeaders -ContentType application/json 
+			if($response2.state -eq 'COMPLETED') {
+				break
 			}
-		}		
-    } 
-    Catch {
-            Write-Error "An error occured. $_"
+		}
+		if($response2.state -eq 'COMPLETED') {
+			write-output "The $product is successfully shutdown"
+		} else {
+			write-output "The $product could not be shutdown within the timeout value"
+		}
     }
-    Finally {
-            Disconnect-VIServer -Server $server -confirm:$false
+    Catch {
+       $PSItem.InvocationInfo
+	   write-error $_
     }
 }
 
 
 
+Function ShutdownStartupProduct-ViaVRSLCM
+{
+    <#
+        .NOTES
+        ===========================================================================
+        Created by:		Sowjanya V
+        Date:			03/16/2021
+        Organization:	VMware
+        ===========================================================================
+        
+        .SYNOPSIS
+        Shutting down VRA or VROPS via VRSLCM
+    
+        .DESCRIPTION
+        There are no POWER CLI or POWER SHELL cmdlet to power off the products via VRSLCM. Hence writing my own
+    
+        .EXAMPLE
+        PS C:\> ShutdownProduct_ViaVRSLCM -host <VRSLCM> -user <username> -pass <password> -component <VRA/VROPS/VRLI/VIDM>
+        This example shutsdown a product via VRSLCM
+        Sample URL formed on TB04 is as shown below
+        Do Get on https://xreg-vrslcm01.rainpole.io/lcm/lcops/api/v2/environments
+           and findout what is the id assosiated with Cross-Region Environment 
 
+        $uri = "https://xreg-vrslcm01.rainpole.io/lcm/lcops/api/v2/environments/Cross-Region-Env1612043838679/products/vra/deployed-vms"
 
+       
+
+    #>
+          
+    Param (
+        [Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$host,
+        [Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$user,
+        [Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$pass,
+        [string]$task,
+        [Parameter (Mandatory=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$product
+
+    )
+    
+    Try {
+        $env_id = Get-EnvironmentId -host $host -Name "Cross-Region"
+		
+		write-output $env_id
+		
+		$Global:myHeaders = createHeader $user $pass
+		write-output $myHeaders
+		if($task) {
+			$uri = "https://$host/lcm/lcops/api/v2/environments/$env_id/products/$product/power-on"
+		} else {
+			$uri = "https://$host/lcm/lcops/api/v2/environments/$env_id/products/$product/power-off"
+		}
+		write-output $uri
+        $json = {}
+        $response = Invoke-RestMethod -Method POST -URI $uri -headers $myHeaders -ContentType application/json -body $json
+		
+		write-output "------------------------------------"
+		write-output $response
+		write-output "------------------------------------"
+        if($response.requestId) {
+            Write-Output "Successfully initiated shutdown of the product $product"
+        } else {
+            Write-Error "Unable to shutdown $product due to response "
+        }
+		$id = $response.requestId
+		$uri2 = "https://$host/lcm/request/api/v2/requests/$id"
+		$count = 0
+		$timeout = 900
+		while($count -le $timeout) {
+			$count = $count + 60
+			start-sleep -s 60
+			$response2 = Invoke-RestMethod -Method GET -URI $uri2 -headers $myHeaders -ContentType application/json 
+			if($response2.state -eq 'COMPLETED') {
+				break
+			}
+		}
+		if($response2.state -eq 'COMPLETED') {
+			write-output "The $product is successfully shutdown"
+		} else {
+			write-output "The $product could not be shutdown within the timeout value"
+		}
+    }
+    Catch {
+       $PSItem.InvocationInfo
+	   write-error $_
+    }
+}
 
