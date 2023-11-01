0fe387f938cf2ab7f6700ac150f296c4c2e83083
Jared Burns <burnsja@vmware.com>
2023-11-01 16:53:09 -0400

    docs: add air-gapped install (#69)
    Adding documentation to install modules and dependencies in a air-gapped environment.
    
    Signed-off-by: Jared Burns <burnsja@vmware.com>

---
+++ docs/snippets/import-module-local-linux.ps1
@@ -0,0 +1,4 @@
+Import-Module -Name VMware.PowerCLI
+Import-Module -Name PowerVCF
+Import-Module -Name Posh-SSH
+Import-Module -Name VMware.CloudFoundation.PowerManagement
---
+++ docs/snippets/save-module-local-windows.ps1
@@ -0,0 +1,4 @@
+Save-Module -Name VMware.PowerCLI -MinimumVersion 13.0.0 -Path F:\Module\
+Save-Module -Name PowerVCF -MinimumVersion 2.3.0 -Path F:\Module\
+Save-Module -Name Posh-SSH -MinimumVersion 3.0.8 -Path F:\Module\
+Save-Module -Name VMware.CloudFoundation.PowerManagement -Path F:\Module\
