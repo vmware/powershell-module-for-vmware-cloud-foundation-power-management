66d61515435710279aa40da8b1d2bdc5c16d6aae
Jared Burns <burnsja@vmware.com>
2023-11-01 16:27:31 -0400

    docs/air-gapped
    Created Process for Air-Gapped Machines for installing Modules.
    
    Signed-off-by: Jared Burns <burnsja@vmware.com>

---
+++ docs/snippets/save-module-local-linux.ps1
@@ -0,0 +1,4 @@
+Save-Module -Name VMware.PowerCLI -MinimumVersion 13.0.0 -Path /home/modules
+Save-Module -Name PowerVCF -MinimumVersion 2.3.0 -Path /home/modules
+Save-Module -Name Posh-SSH -MinimumVersion 3.0.8 -Path /home/modules
+Save-Module -Name VMware.CloudFoundation.PowerManagement -Path /home/modules
