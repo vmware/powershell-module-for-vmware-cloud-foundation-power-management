4942f89732ca2cbf73cd573c836a8177df0ce3d0
Jared Burns <burnsja@vmware.com>
2023-11-16 08:06:10 -0500

    :docs/update_install_save_module_add_repository
    Updated Install and Save Module with Repository PSGallery flag
    
    Signed-off-by: Jared Burns <burnsja@vmware.com>

--- docs/snippets/install-module.ps1
+++ docs/snippets/install-module.ps1
@@ -1,5 +1,5 @@
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name VMware.PowerCLI -MinimumVersion 13.0.0
-Install-Module -Name PowerVCF -MinimumVersion 2.3.0
-Install-Module -Name Posh-SSH -MinimumVersion 3.0.8
-Install-Module -Name VMware.CloudFoundation.PowerManagement
+Install-Module -Name VMware.PowerCLI -MinimumVersion 13.0.0 -Repository PSGallery
+Install-Module -Name PowerVCF -MinimumVersion 2.3.0 -Repository PSGallery
+Install-Module -Name Posh-SSH -MinimumVersion 3.0.8 -Repository PSGallery
+Install-Module -Name VMware.CloudFoundation.PowerManagement -Repository PSGallery
